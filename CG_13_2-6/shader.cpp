314dc4cdb26a9ad64e3d1c67d4cd9d07af0af0eb
rexsot <112843175+rexsot@users.noreply.github.com>
2022-10-11 03:33:12 +0900

    셰이더-미완

---
+++ CG_13_2-6/shader.cpp
@@ -0,0 +1,215 @@
+﻿// glew32.lib freeglut.lib
+// 파일 -> 새로만들기 -> 파일 -> c++ -> glsl파일 만들기
+#pragma warning(disable:4996)
+
+#include <iostream>
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h>
+
+char* filetobuf(const char* file);
+GLvoid drawScene();
+GLvoid Reshape(int w, int h);
+
+void make_vertexShaders();
+void make_fragmentShaders();
+void make_shaderProgram();
+void InitBuffer();
+
+GLint width, height;
+GLint vertexShader;
+GLuint fragmentShader;
+GLuint shaderProgram;
+GLuint VAO, VBO[2];
+
+float vertexPosition[] = {
+    0.5, 1.0, 0.0,
+    0.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+
+    -0.5, 1.0, 0.0,
+    -1.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+
+    -0.5, 0.0, 0.0,
+    -1.0, -1.0, 0.0,
+    0.0, -1.0, 0.0,
+
+    0.5, 0.0, 0.0,
+    1.0, -1.0, 0.0,
+    0.0, -1.0, 0.0
+};
+
+float vertexColor[] = {
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+    0.0, 1.0, 0.0,
+
+    0.0, 0.0, 1.0,
+    0.0, 0.0, 1.0,
+    0.0, 0.0, 1.0,
+
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    1.0, 1.0, 1.0
+
+};
+
+void main(int argc, char** argv)
+{
+    width = 500;
+    height = 500;
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(width, height);
+    glutCreateWindow("Example1");
+
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    make_vertexShaders();
+    make_fragmentShaders();
+    make_shaderProgram();
+
+    InitBuffer();
+
+    glutDisplayFunc(drawScene);
+    glutReshapeFunc(Reshape);
+    //glutMouseFunc(Mouse);
+
+    glutMainLoop();
+}
+
+GLvoid drawScene()
+{
+    GLfloat rColor, gColor, bColor;
+    rColor = gColor = bColor = 0.0;
+    glClearColor(rColor, gColor, bColor, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 12);
+    //glDrawArrays(GL_LINES, 3, 2); 
+    glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+}
+
+// GLuint VAO, VBO[2];
+// VBO_pos[n] 꼴로 여러개 사용 가능
+
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+//GLint vertexShader;
+//GLuint fragmentShader;
+
+GLvoid InitBuffer()
+{
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    
+    glGenBuffers(2, VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPosition), vertexPosition, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexColor), vertexColor, GL_STATIC_DRAW);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(1);
+
+}
+
+void make_vertexShaders()
+{
+    vertexSource = filetobuf("vertex.glsl");
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, (const GLchar**)&vertexSource, 0);
+    glCompileShader(vertexShader);
+
+
+    GLint result;
+    GLchar errorLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, errorLog);
+        std::cerr << "ERROR: vertex shader 컴파일 실패\n" << errorLog << std::endl;
+        return;
+    }
+}
+
+void make_fragmentShaders()
+{
+    fragmentSource = filetobuf("fragment.glsl");
+
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, (const GLchar**)&fragmentSource, 0);
+    glCompileShader(fragmentShader);
+
+
+
+    GLint result;
+    GLchar errorLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &result);
+    if (!result)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, errorLog);
+        std::cerr << "ERROR: fragment shader 컴파일 실패\n" << errorLog << std::endl;
+        return;
+    }
+}
+
+void make_shaderProgram()
+{
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    GLint result;
+    GLchar errorLog[512];
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+    if (!result) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, errorLog);
+        std::cerr << "ERROR: shader program 연결 실패\n" << errorLog << std::endl;
+        return;
+    }
+    glUseProgram(shaderProgram);
+}
+
+char* filetobuf(const char* file)
+{
+    FILE* fptr;
+    long length;
+    char* buf;
+
+    fptr = fopen(file, "rb");
+    if (!fptr)
+        return NULL;
+    fseek(fptr, 0, SEEK_END);
+    length = ftell(fptr);
+    buf = (char*)malloc(length + 1);
+    fseek(fptr, 0, SEEK_SET);
+    fread(buf, length, 1, fptr);
+    fclose(fptr);
+    buf[length] = 0;
+
+    return buf;
+}
