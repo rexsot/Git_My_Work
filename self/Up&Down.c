4546f33dd01096df6d54efcdd62dd5da40874f87
rexsot <112843175+rexsot@users.noreply.github.com>
2022-10-12 00:57:38 +0900

    증가/감소

---
+++ self/Up&Down.c
@@ -0,0 +1,21 @@
+﻿#pragma warning(disable:4996)
+#include<stdio.h>
+
+int main()
+{
+	// 1234543212345....
+	// 1234 5 432 1
+
+	int n = 1;
+	int boo = -1;
+
+	for (int i = 0; i < 20; i++) {
+
+		if (n == 1 || n == 5) {
+			boo *= -1;
+		}
+		printf("%d\n", n);
+		n += boo;
+	}
+	return 0;
+}
